9b52d7dcea94aa5013b963b30424ca911721fba0
TGpaty <3116315908@qq.com>
2022-05-09 22:39:42 +0800

    添加项目文件。

---
+++ Project1/main.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	unsigned int a = (10, (2, 34, 5, 6, 7, 7, 8), 3, 4, 5, 6, (2, 3, 45, 6, 7, -7));
+
+	printf("%d\n", a);
+	
+
+	return EXIT_SUCCESS;
+}
